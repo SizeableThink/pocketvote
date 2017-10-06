25e49a5a5f11dd01c06f6ada20c24e3ccb5eb71e
Sink <esink@luc.edu>
2017-10-06 15:06:53 -0500

    trashed template info on background, fixed plurality

--- js/app/plurality.js
+++ js/app/plurality.js
@@ -1,7 +1,53 @@
-'use strict';
-
 console.log("Loaded plurality.js");
 
 var tallyPlurality = function(ballots) {
-	tallyPlurality(ballots);
+	var voteCounts = countChoice(ballots);
+	var highestSoFar = -Infinity;
+	var winner = [];
+	console.log(voteCounts);
+	for (var choice in voteCounts) {
+		console.log("choice", choice);
+		if (voteCounts.hasOwnProperty(choice)){
+			var v = voteCounts[choice];
+			console.log("v", v);
+			if (v > highestSoFar) {
+				highestSoFar = voteCounts[choice];
+				console.log("highestSoFar", highestSoFar);
+			}
+		}
+	}	
+	for (var choice in voteCounts) {
+		if (voteCounts.hasOwnProperty(choice)) {
+			var v = voteCounts[choice];
+			if (v === highestSoFar) {
+				winner.push(choice);
+			}
+		}
+	}
+	console.log(winner, voteCounts);
+	return [winner, voteCounts];
+};
+
+var countsList = function(ballots){
+	var voteCountsList = [];
+	console.log("ballots", ballots)
+	ballots.forEach(function(ballot) {
+		console.log("ballot.choiceSelections", ballot.choiceSelections)
+		for (var i in ballot.choiceSelections){
+			if (ballot.choiceSelections[i].selection === true){
+				voteCountsList.push(ballot.choiceSelections[i].choice);
+			}
+		}
+	});
+	return voteCountsList;
+}
+
+var countChoice = function(ballots) {
+	var voteCountsList = countsList(ballots);
+	console.log("voteCountsList", voteCountsList)
+	var voteCountDictionary = {};
+	for (var i = 0; i < voteCountsList.length; i++) {
+		voteCountDictionary[voteCountsList[i]] = (voteCountDictionary[voteCountsList[i]] || 0) + 1;
+	}
+	return voteCountDictionary;
 }
