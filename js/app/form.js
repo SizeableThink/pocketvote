c931f7e410c7f1255deeb04494be4c06cf84354c
Suchitra Manivannan <suchimani@Suchitras-MacBook-Pro.local>
2017-11-05 23:32:12 -0600

    Create Ballot Page, Login Page modified and added

---
+++ js/app/form.js
@@ -0,0 +1,57 @@
+$(document).ready(function(){
+
+  var counter = 1;
+
+  $("#addButton").click(function () {
+
+  	   $('#add_textbox').append('<input id="input_size" class="choice_ballot_txt' + counter + '" type="text" name="text2" ng-model="newChoice"><br>');  
+      counter++;
+  });
+
+
+  $("#remvButton").click(function () {
+	if(counter==1){
+          alert("No more textboxes to remove");
+          return false;
+       }
+
+	counter--;
+	console.log(counter);
+
+        $(".choice_ballot_txt" + counter).remove();
+
+     });
+
+  	 // $('#lbl_form').validate({ // initialize the plugin
+    //     rules: {
+    //         text1: {
+    //             required: true,
+    //             minlength: 5
+    //         },
+    //         text2: {
+    //             required: true,
+    //             minlength: 5
+    //         }
+    //     }
+    // });
+
+
+
+  	 $('#login-form-link').click(function(e) {
+		$("#login-form").delay(100).fadeIn(100);
+ 		$("#register-form").fadeOut(100);
+		$('#register-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+  	 
+	$('#register-form-link').click(function(e) {
+		$("#register-form").delay(100).fadeIn(100);
+ 		$("#login-form").fadeOut(100);
+		$('#login-form-link').removeClass('active');
+		$(this).addClass('active');
+		e.preventDefault();
+	});
+
+
+});
