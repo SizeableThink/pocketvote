f200fbd2cf1e25c08e0308c1d738319835f98c79
Liz Sink <sink217@gmail.com>
2017-12-10 09:12:38 -0600

    reintroduced angular for demo page

--- js/app/app.js
+++ js/app/app.js
@@ -10,29 +10,7 @@ var app = angular.module('ballotApp', []);
 app.controller('ballotCtrl', function($scope) {
 
 	//console.log($scope);
-	$scope.ballotTypes = ballotTypes;
-
-	$scope.change = function() {
-    	return;
-    }
-
-    /**
-	//Declaring choices (array literal)
-	*/
-
-	$scope.choices = [];
-
- 	//When creating ballot this allows user to add a choice by pushing the string onto the array
-	$scope.addChoice = function () {
-		var resetText = null;
-		if ($scope.newChoice != resetText) {
-			$scope.choices.push($scope.newChoice);
-			$scope.newChoice = resetText;
-		}
-		//console.log("Logging addChoice")
-	};
-
-	var ballotTypes = [{	
+	$scope.ballotTypes = [{	
 		name: "Ranked Choice",
 	}, /*{
 		name: "Range Choice",
@@ -42,11 +20,38 @@ app.controller('ballotCtrl', function($scope) {
 		name: "Plurality",
 	}];
 
- 	//Declaring ballots (array literal)
+	$scope.change = function() {
+    	return;
+    }
+
+	/**
+ 	* Declaring choices (array literal)
+ 	*/
+
+	$scope.choices = [];
+
+	/**
+ 	* When creating ballot this allows user to add a choice by pushing the string onto the array
+ 	*/
+
+	$scope.addChoice = function () {
+		var resetText = null;
+		if ($scope.newChoice != resetText) {
+			$scope.choices.push($scope.newChoice);
+			$scope.newChoice = resetText;
+		}
+		//console.log("Logging addChoice")
+	};
+
+	/**
+ 	* Declaring ballots (array literal)
+ 	*/
 
 	$scope.ballots = [];
 
-	//Using the input from users creates the number of ballots requested
+	/**
+ 	* Using the input from users creates the number of ballots requested
+ 	*/
 
 	$scope.addBallot = function() {
 		for(var i=0; i < $scope.ballotNumber; i++) {
@@ -65,7 +70,9 @@ app.controller('ballotCtrl', function($scope) {
 		}
 	}
 
- 	//To reload page
+	/**
+ 	* To reload page
+ 	*/
 
 	$scope.reloadRoute = function() {
    		$window.location.reload();
@@ -87,37 +94,379 @@ app.controller('ballotCtrl', function($scope) {
  	* Takes in all user ballots and depending on ballot chosen finds the winner
  	*/
 
-	$scope.findwinner = findwinner;
+	$scope.findwinner = function() {
+		//console.log($scope.ballots);
+		var ballotsCopy = $.map($scope.ballots, function (ballot) {
+			return $.extend(true, {}, ballot);
+		});
+		//console.log(ballotsCopy);
+		switch($scope.selectedBallotType) {
+    		case "Ranked Choice":
+        		var results = tallyRankedChoice(ballotsCopy);
+        		$scope.winner = results[0];
+        		$scope.history = results[1];
+        		break;
+    		/*case "Range Choice":
+        		$scope.winner = tallyRangeChoice();
+        		break;*/
+        	case "Approval":
+        		var results = tallyApproval(ballotsCopy);
+        		$scope.winner = results[0];
+        		$scope.counts = results[1];
+        		break;
+        	case "Plurality":
+        		var results = tallyPlurality(ballotsCopy);
+        		$scope.winner = results[0];
+        		$scope.counts = results[1];
+        		break;
+    		default:
+        		return null;
+		}
+	}
 
 	/**
  	* Allows plurality to be used on checkboxes and only have one checkbox marked at a time
  	*/
 
-	$scope.updateSelection = updateSelection;
+	 $scope.updateSelection = function(position, choiceSelections) {
+      angular.forEach(choiceSelections, function(choiceSelection, index) {
+      	//console.log("position, choiceSelections", position, choiceSelections);
+      	//console.log("choiceSelection, index", choiceSelection, index);
+        if (position != index) {
+        	console.log("choiceSelection.checked", choiceSelection.checked);
+          choiceSelection.selection = false;
+      	}
+      });
+    }
 
     /**
  	* Demos ranked choice ballot
  	*/
 
-	$scope.testRankedChoice = testRankedChoice; 
+	$scope.testRankedChoice = function() {
+		$scope.choices = ["vanilla", "chocolate", "rocky road", "strawberry"];
+		$scope.ballotName = "Ice Cream";
+		$scope.selectedBallotType = "Ranked Choice";
+		$scope.ballotNumber = 5;
+		$scope.ballots = [];
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: 1,
+				},
+				{
+					choice: "chocolate",
+					selection: 3,
+				},
+				{
+					choice: "rocky road",
+					selection: 2,
+				},
+				{
+					choice: "strawberry",
+					selection: 4,
+				}]});
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: 4,
+				},
+				{
+					choice: "chocolate",
+					selection: 3,
+				},
+				{
+					choice: "rocky road",
+					selection: 1,
+				},
+				{
+					choice: "strawberry",
+					selection: 2,
+				}]});
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: 4,
+				},
+				{
+					choice: "chocolate",
+					selection: 1,
+				},
+				{
+					choice: "rocky road",
+					selection: 3,
+				},
+				{
+					choice: "strawberry",
+					selection: 2,
+				}]});
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: 4,
+				},
+				{
+					choice: "chocolate",
+					selection: 3,
+				},
+				{
+					choice: "rocky road",
+					selection: 1,
+				},
+				{
+					choice: "strawberry",
+					selection: 2,
+				}]});
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: 4,
+				},
+				{
+					choice: "chocolate",
+					selection: 1,
+				},
+				{
+					choice: "rocky road",
+					selection: 2,
+				},
+				{
+					choice: "strawberry",
+					selection: 3,
+				}]});
+	}
 
 	/**
  	* Demos approval ballot
  	*/
 
-	$scope.testApproval = testApproval;
+	$scope.testApproval = function() {
+		$scope.choices = ["vanilla", "chocolate", "rocky road", "strawberry"];
+		$scope.ballotName = "Ice Cream";
+		$scope.selectedBallotType = "Approval";
+		$scope.ballotNumber = 5;
+		$scope.ballots = [];
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: true,
+				},
+				{
+					choice: "chocolate",
+					selection: false,
+				},
+				{
+					choice: "rocky road",
+					selection: true,
+				},
+				{
+					choice: "strawberry",
+					selection: false,
+				}]});
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: true,
+				},
+				{
+					choice: "chocolate",
+					selection: true,
+				},
+				{
+					choice: "rocky road",
+					selection: false,
+				},
+				{
+					choice: "strawberry",
+					selection: false,
+				}]});
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: false,
+				},
+				{
+					choice: "chocolate",
+					selection: true,
+				},
+				{
+					choice: "rocky road",
+					selection: true,
+				},
+				{
+					choice: "strawberry",
+					selection: true,
+				}]});
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: false,
+				},
+				{
+					choice: "chocolate",
+					selection: true,
+				},
+				{
+					choice: "rocky road",
+					selection: false,
+				},
+				{
+					choice: "strawberry",
+					selection: false,
+				}]});
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: true,
+				},
+				{
+					choice: "chocolate",
+					selection: true,
+				},
+				{
+					choice: "rocky road",
+					selection: true,
+				},
+				{
+					choice: "strawberry",
+					selection: true,
+				}]});
+	}
 
 	/**
  	* Demos plurality ballot
  	*/	
 
-	$scope.testPlurality = testPlurality;
+	$scope.testPlurality = function() {
+		$scope.choices = ["vanilla", "chocolate", "rocky road", "strawberry"];
+		$scope.ballotName = "Ice Cream";
+		$scope.selectedBallotType = "Plurality";
+		$scope.ballotNumber = 5;
+		$scope.ballots = [];
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: true,
+				},
+				{
+					choice: "chocolate",
+					selection: false,
+				},
+				{
+					choice: "rocky road",
+					selection: false,
+				},
+				{
+					choice: "strawberry",
+					selection: false,
+				}]});
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: false,
+				},
+				{
+					choice: "chocolate",
+					selection: true,
+				},
+				{
+					choice: "rocky road",
+					selection: false,
+				},
+				{
+					choice: "strawberry",
+					selection: false,
+				}]});
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: false,
+				},
+				{
+					choice: "chocolate",
+					selection: false,
+				},
+				{
+					choice: "rocky road",
+					selection: true,
+				},
+				{
+					choice: "strawberry",
+					selection: false,
+				}]});
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: false,
+				},
+				{
+					choice: "chocolate",
+					selection: true,
+				},
+				{
+					choice: "rocky road",
+					selection: false,
+				},
+				{
+					choice: "strawberry",
+					selection: false,
+				}]});
+		$scope.ballots.push({
+				name: $scope.ballotName,
+				type: $scope.selectedBallotType,
+				choiceSelections: [{
+					choice: "vanilla",
+					selection: false,
+				},
+				{
+					choice: "chocolate",
+					selection: false,
+				},
+				{
+					choice: "rocky road",
+					selection: false,
+				},
+				{
+					choice: "strawberry",
+					selection: true,
+				}]});
+	}
 
 	/**
  	* returns the checked state of a checkbox
  	*/
 
-	var check = check;
+	var check = function(element) {
+    document.getElementById(element).checked = true;
+	}
 
 	/**
  	* Placeholder for expansion
